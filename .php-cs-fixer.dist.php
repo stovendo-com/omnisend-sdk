022b6c7c8197ef8b22b9db55c71a43f178463699
Jacek Kobus <kobus.jacek@gmail.com>
2024-06-13 20:04:58 +0200

    Update licensing information

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -8,18 +8,7 @@ $finder = (new PhpCsFixer\Finder())
 ;
 
 $header = <<<EOF
-@copyright © UAB NFQ Technologies
-
-This Software is the property of NFQ Technologies
-and is protected by copyright law – it is NOT Freeware.
-
-Any unauthorized use of this software without a valid license key
-is a violation of the license agreement and will be prosecuted by
-civil and criminal law.
-
-Contact UAB NFQ Technologies:
-E-mail: info@nfq.lt
-https://www.nfq.lt
+(c) NFQ Technologies UAB <info@nfq.com>
 EOF;
 
 
