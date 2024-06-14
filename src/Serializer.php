e1331c73d03742727d2e033a55dcd832c8e6dcc5
Jacek <kobus.jacek@gmail.com>
2024-06-14 10:42:13 +0300

    Update licensing information (#3)

--- src/Serializer.php
+++ src/Serializer.php
@@ -1,18 +1,7 @@
 <?php
 
 /**
- * @copyright © UAB NFQ Technologies
- *
- * This Software is the property of NFQ Technologies
- * and is protected by copyright law – it is NOT Freeware.
- *
- * Any unauthorized use of this software without a valid license key
- * is a violation of the license agreement and will be prosecuted by
- * civil and criminal law.
- *
- * Contact UAB NFQ Technologies:
- * E-mail: info@nfq.lt
- * https://www.nfq.lt
+ * (c) NFQ Technologies UAB <info@nfq.com>
  */
 
 declare(strict_types=1);
